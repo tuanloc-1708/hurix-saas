4f7463884f3af675d25d6dce05823f0de52778bc
tuanloc-1708 <97837536+tuanloc-1708@users.noreply.github.com>
2024-11-15 11:11:04 +0700

    fix all src

--- src/components/heading.tsx
+++
@@ -1,9 +0,0 @@
-type HeadingProps = {
-  title: string;
-};
-
-const Heading = ({ title }: HeadingProps) => {
-  return <h1 className="text-2xl font-bold text-center">{title}</h1>;
-};
-
-export default Heading;
--- src/pages/index.tsx
+++
@@ -1,9 +0,0 @@
-import Heading from "../components/heading";
-
-export default function Home() {
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <Heading title="Chào mừng bạn đến với Next.js + Tailwind CSS + TypeScript!" />
-    </div>
-  );
-}
