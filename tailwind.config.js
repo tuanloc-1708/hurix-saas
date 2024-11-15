cb1ba8cf7ed7d3146e92e43759156a427636f108
tuanloc-1708 <97837536+tuanloc-1708@users.noreply.github.com>
2024-11-15 10:34:07 +0700

    update tailwin config & arrange file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./pages/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}"],
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "top-pink-gradient":
+          "linear-gradient(159.54deg, #FEECE3 -26.35%, #FCD5BF 11.82%, #FEAFAE 50%, #FFA4BD 88.18%, #FFA9CC 126.35%)",
+        "left-pink-gradient":
+          "linear-gradient(110.45deg, #FEECE3 0%, #FCD5BF 25%, #FEAFAE 50%, #FFA4BD 75%, #FFA9CC 100%)",
+      },
+    },
   },
   plugins: [],
 };
